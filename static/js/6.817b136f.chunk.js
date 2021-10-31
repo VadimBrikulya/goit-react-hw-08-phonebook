27d86916cfa48fc2923997e2552db3a2f96df1b4
Vadim Brikulya <im.master99@gmail.com>
2021-10-31 21:43:31 +0200

    Updates

---
+++ static/js/6.817b136f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[6],{108:function(e,t,o){"use strict";o.r(t);o(0);var n=o(91),i=o.n(n),c=o(1);t.default=function(){return Object(c.jsx)("div",{className:i.a.container,children:Object(c.jsx)("h1",{className:i.a.title,children:"Welcome to Phonebook"})})}},91:function(e,t,o){e.exports={container:"HomeViews_container__3p2rQ",title:"HomeViews_title__2G0GJ"}}}]);
+//# sourceMappingURL=6.817b136f.chunk.js.map
